68ca565ca0499e1efb61e22132fc44534eeed2f6
SuitableEmu <73889862+SuitableEmu@users.noreply.github.com>
2021-10-08 10:59:28 +0200

    Update SQL Backup script.ps1

--- SQL Backup script.ps1
+++ SQL Backup script.ps1
@@ -1,4 +1,6 @@
-﻿ Backup-SqlDatabase -ServerInstance "APP\SQL Server Name" -Database "Database Name"
+#You need to setup an "Backup Device" in SQL Management Studio before running the script
+
+Backup-SqlDatabase -ServerInstance "APP\SQL Server Name" -Database "Database Name"
 
  Start-Sleep -s 3
  
@@ -8,4 +10,4 @@ $filenameFormat = "Name of backup" + " " + (Get-Date -Format "dd-MM-yyyy") + ".b
 
 Move-Item -Path 'Location.bak' -Destination 'Destination'
 Start-Sleep -s 3
-Rename-Item -Path 'New path and name' -NewName $filenameFormat
+Rename-Item -Path 'New path' -NewName $filenameFormat
