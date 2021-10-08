e597472509006ad36619c7a95805dc6bf7b0bef5
SuitableEmu <73889862+SuitableEmu@users.noreply.github.com>
2021-10-08 10:57:59 +0200

    Add files via upload
    Simple SQL Backup script
    
    You need to set up a "Backup Device" in SQL Management
    
    Should work for any SQL version

---
+++ SQL Backup script.ps1
@@ -0,0 +1,11 @@
+﻿ Backup-SqlDatabase -ServerInstance "APP\SQL Server Name" -Database "Database Name"
+
+ Start-Sleep -s 3
+ 
+
+$filenameFormat = "Name of backup" + " " + (Get-Date -Format "dd-MM-yyyy") + ".bak"
+
+
+Move-Item -Path 'Location.bak' -Destination 'Destination'
+Start-Sleep -s 3
+Rename-Item -Path 'New path and name' -NewName $filenameFormat
