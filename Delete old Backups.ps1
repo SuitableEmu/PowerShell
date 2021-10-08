edff60baa5fd5b5830c4dd93bfb9373d845b6167
SuitableEmu <73889862+SuitableEmu@users.noreply.github.com>
2021-10-08 10:52:40 +0200

    Add files via upload
    Deletes old backups

---
+++ Delete old Backups.ps1
@@ -0,0 +1,19 @@
+﻿#where the backups are located
+$Folder = "Location"
+
+#Delete files older than 6 months (or how long you want to keep the backup's 360 = 1 year 180 = 6 months etc)
+Get-ChildItem $Folder -Recurse -Force -ea 0 |
+? {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-180)} |
+ForEach-Object {
+   $_ | del -Forc
+   $_.FullName | Out-File 'Location\deletedlog.txt' -Append
+}
+
+#Delete empty folders and subfolders
+Get-ChildItem $Folder -Recurse -Force -ea 0 |
+? {$_.PsIsContainer -eq $True} |
+? {$_.getfiles().count -eq 0} |
+ForEach-Object {
+    $_ | del -Force
+    $_.FullName | Out-File 'Location\deletedlog.txt' -Append
+}
